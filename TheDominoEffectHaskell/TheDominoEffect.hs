316df30c4cdbe95b28f31cec285d6112c711c621
Janine Klein Rot <janine.kleinrot@nedap.com>
2018-09-17 10:01:34 +0200

    Haskell: Input is asked for and checked for validity

--- TheDominoEffectHaskell/TheDominoEffect.hs
+++ TheDominoEffectHaskell/TheDominoEffect.hs
@@ -1,1 +1,37 @@
 -- The Domino Effect
+
+import System.IO
+import Data.Char
+
+-- The main function
+theDominoEffect :: IO ()
+theDominoEffect = do putStrLn "Please enter the 7 x 8 domino grid. Enter each number starting at the top left number and work your way down row wise to the bottom right number of the grid: "
+                     input <- getLine
+                     if (isValidInput input) then 
+                            putStrLn input
+                     else 
+                            do putStrLn "The entered domino grid was not valid."
+                               theDominoEffect
+
+-- Checks whether the input grid is valid
+isValidInput :: String -> Bool
+isValidInput input = if not (areCorrectDigits input) then 
+                             False 
+                     else if length input /= 56 then 
+                             False 
+                     else if not (isValidGrid input) then
+                             False
+                     else 
+                             True
+
+-- Checks whether the input grid only consists of the digits 0..6
+areCorrectDigits :: String -> Bool
+areCorrectDigits [] = True
+areCorrectDigits (x:xs) = isDigit x && digitToInt x <= 6 && areCorrectDigits xs
+
+-- Checks whether the input grid contains each valid digit eight times
+isValidGrid :: String -> Bool
+isValidGrid input = length (filter (== '0') input) == 8 && length (filter (== '1') input) == 8 && length (filter (== '2') input) == 8 && length (filter (== '3') input) == 8 &&
+                    length (filter (== '4') input) == 8 && length (filter (== '5') input) == 8 && length (filter (== '6') input) == 8
+
+
