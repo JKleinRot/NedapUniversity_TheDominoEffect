c9dab345effda14807ade2727e41e29d0b82f178
Janine Klein Rot <janine.kleinrot@nedap.com>
2018-09-17 11:49:35 +0200

    Haskell: Board created from input grid

--- TheDominoEffectHaskell/TheDominoEffect.hs
+++ TheDominoEffectHaskell/TheDominoEffect.hs
@@ -5,16 +5,21 @@ import Data.Char
 
 -- A domino stone is a tuple with two pips
 type Dom = (Int, Int)
+-- A position is a tuple with the pip, whether or not the pip is occupied and the bone it is occupied with, if the pip is not occupied, the pip is -1
+type Pos = (Int, Bool, Int)
+-- A board is a list with a list of positions
+type Board = [[Pos]]
 
 -- The main function
 theDominoEffect :: IO ()
 theDominoEffect = do putStrLn "Please enter the 7 x 8 domino grid. Enter each number starting at the top left number and work your way down row wise to the bottom right number of the grid: "
                      input <- getLine
                      if (isValidInput input) then 
-                            putStrLn input
+                             do let board = createBoard(input)
+                                putStrLn (show board)
                      else 
-                            do putStrLn "The entered domino grid was not valid."
-                               theDominoEffect
+                             do putStrLn "The entered domino grid was not valid."
+                                theDominoEffect
 
 -- Checks whether the input grid is valid
 isValidInput :: String -> Bool
@@ -46,3 +51,17 @@ dominos = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6),
                                            (4, 4), (4, 5), (4, 6),
                                                    (5, 5), (5, 6),
                                                            (6, 6)]
+
+-- Creates a board from the input grid
+createBoard :: String -> Board
+createBoard input = map createInitialPositions (splitToRows input)
+
+-- Splits the input grid into rows
+splitToRows :: String -> [String]
+splitToRows [] = []
+splitToRows input = [take 8 input] ++ splitToRows (drop 8 input)
+
+-- Creates the initial positions of the board from the rows of the input grid
+createInitialPositions :: String -> [Pos]
+createInitialPositions [] = []
+createInitialPositions (pos:row) = ((digitToInt pos), False, -1) : createInitialPositions row
